3ebddfc246c3b4b0566fb21a131bfd976cf89cbc
liuzhengqiu <liuzhengqiu1127@163.com>
2019-10-11 15:02:35 +0800

    update

--- python_algorithms/leetcode/medium/Solution.py
+++ python_algorithms/leetcode/medium/Solution.py
@@ -1,20 +1,27 @@
 class Solution:
 
+    def __init__(self):
+        pass
+
     def permute(self, nums):
-        def backtrack(first = 0):
+        def backtrack(first=0):
             if first == n:
                 output.append(nums[:])
             for i in range(first, n):
                 nums[first], nums[i] = nums[i], nums[first]
-                backtrack(first+1)
+                backtrack(first + 1)
                 nums[first], nums[i] = nums[i], nums[first]
+
         n = len(nums)
         output = []
         backtrack()
         return output
 
-    if __name__ == '__main__':
-        nums = [1,2,3]
-        output = permute(nums)
-        for out in output:
-            print(out)
+
+if __name__ == '__main__':
+
+    nums = [1, 2, 3]
+    solution = Solution()
+    output = solution.permute(nums)
+    for out in output:
+        print(out)
