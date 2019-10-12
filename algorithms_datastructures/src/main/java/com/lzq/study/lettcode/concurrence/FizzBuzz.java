a949472b6c629f32e6c10848005bc2c95e836124
liuzhengqiu <liuzhengqiu1127@163.com>
2019-10-12 23:04:22 +0800

    解决高并发的几个问题

--- algorithms_datastructures/src/main/java/com/lzq/study/lettcode/concurrence/FizzBuzz.java
+++ algorithms_datastructures/src/main/java/com/lzq/study/lettcode/concurrence/FizzBuzz.java
@@ -1,9 +1,11 @@
 package com.lzq.study.lettcode.concurrence;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.IntConsumer;
 
 public class FizzBuzz {
     private int n;
+    AtomicInteger atomicInteger = new AtomicInteger(1);
 
     public FizzBuzz(int n) {
         this.n = n;
@@ -11,24 +13,60 @@ public class FizzBuzz {
 
     // printFizz.run() outputs "fizz".
     public void fizz(Runnable printFizz) throws InterruptedException {
-
+        synchronized (this){
+            while (atomicInteger.get() <= n){
+                while(atomicInteger.get() % 3 != 0 || atomicInteger.get() % 15 == 0){
+                    this.wait();
+                    if (atomicInteger.get() > n) return;
+                }
+                printFizz.run();
+                atomicInteger.incrementAndGet();
+                this.notifyAll();
+            }
+        }
     }
 
     // printBuzz.run() outputs "buzz".
     public void buzz(Runnable printBuzz) throws InterruptedException {
-
+        synchronized (this){
+            while (atomicInteger.get() <= n){
+                while(atomicInteger.get() % 5 != 0 || atomicInteger.get() % 15 == 0){
+                    this.wait();
+                    if (atomicInteger.get() > n) return;
+                }
+                printBuzz.run();
+                atomicInteger.incrementAndGet();
+                this.notifyAll();
+            }
+        }
     }
 
     // printFizzBuzz.run() outputs "fizzbuzz".
     public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
-
+        synchronized (this){
+            while (atomicInteger.get() <= n){
+                while(atomicInteger.get() % 15 != 0 ){
+                    this.wait();
+                    if (atomicInteger.get() > n) return;
+                }
+                printFizzBuzz.run();
+                atomicInteger.incrementAndGet();
+                this.notifyAll();
+            }
+        }
     }
 
     // printNumber.accept(x) outputs "x", where x is an integer.
     public void number(IntConsumer printNumber) throws InterruptedException {
         synchronized (this){
-            for (int i=1; i<=n; i++){
-                printNumber.accept(i);
+            while (atomicInteger.get() <= n){
+                while(atomicInteger.get() % 3 == 0 || atomicInteger.get() % 5 == 0){
+                    this.wait();
+                    if(atomicInteger.get() > n) return;
+                }
+                printNumber.accept(atomicInteger.get());
+                atomicInteger.incrementAndGet();
+                this.notifyAll();
             }
         }
     }
