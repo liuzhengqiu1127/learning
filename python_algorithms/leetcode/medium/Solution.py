b8209afcf1b56610851a9aba0606a60e78729476
liuzhengqiu <liuzhengqiu1127@163.com>
2019-10-12 08:30:08 +0800

    update

--- python_algorithms/leetcode/medium/Solution.py
+++ python_algorithms/leetcode/medium/Solution.py
@@ -17,11 +17,37 @@ class Solution:
         backtrack()
         return output
 
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums) == 0:
+            return []
+        nums.sort()
+        used = [False] * len(nums)
+        res = []
+        self.__dfs(nums,0,[],used,res)
+        return res
+    def __dfs(self, nums, index, pre, used, res):
+        if index == len(nums):
+            res.append(pre.copy())
+            return
+        for i in range(len(nums)):
+            if not used[i]:
+                if i > 0 and nums[i] == nums[i-1] and not used[i-1]:
+                    continue
+                used[i] = True
+                pre.append(nums[i])
+                self.__dfs(nums,index+1,pre,used,res)
+                used[i] = False
+                pre.pop()
+
 
 if __name__ == '__main__':
 
-    nums = [1, 2, 3]
+    nums = [2,2,1,1]
     solution = Solution()
-    output = solution.permute(nums)
+    output = solution.permuteUnique(nums)
     for out in output:
         print(out)
