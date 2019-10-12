0c381b1e598bde1e2df024a521ca528773590313
liuzhengqiu <liuzhengqiu1127@163.com>
2019-10-12 11:07:59 +0800

    add python test

--- python_algorithms/leetcode/medium/Solution.py
+++ python_algorithms/leetcode/medium/Solution.py
@@ -1,3 +1,6 @@
+import collections
+from typing import List
+
 class Solution:
 
     def __init__(self):
@@ -17,7 +20,7 @@ class Solution:
         backtrack()
         return output
 
-    def permuteUnique(self, nums):
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         """
         :type nums: List[int]
         :rtype: List[List[int]]
@@ -44,11 +47,47 @@ class Solution:
                 used[i] = False
                 pre.pop()
 
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        length = len(matrix)
+        for i in range(length):
+            for j in range(i, length):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+        for i in range(length):
+            matrix[i].reverse()
+
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        ans = collections.defaultdict(list)
+        for s in strs:
+            ans[tuple(sorted(s))].append(s)
+        return ans.values()
+
+    def myPow(self, x: float, n: int) -> float:
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n < 0:
+            x = 1 / x
+        return self.myPowHelper(x * x, int(n/2), 1) if n % 2 == 0 else self.myPowHelper(x * x, int(n/2), x)
+
+    def myPowHelper(self, x, n, re) -> float:
+        if n == 0:
+            return re
+        elif n == 1:
+            return x * re;
+        else:
+            return self.myPowHelper(x * x, int(n/2), re) if n % 2 == 0 else self.myPowHelper(x * x, int(n/2), x * re)
 
 if __name__ == '__main__':
 
-    nums = [2, 2, 1, 1]
     solution = Solution()
-    output = solution.permuteUnique(nums)
-    for out in output:
-        print(out)
+    output = solution.myPow(2.0, 10)
+    print(output)
