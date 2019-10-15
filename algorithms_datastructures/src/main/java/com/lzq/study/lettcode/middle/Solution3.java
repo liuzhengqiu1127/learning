2069e0fb1c70479336816859c6b0ae1fac5b5cac
liuzhengqiu <liuzhengqiu1127@163.com>
2019-10-15 10:15:56 +0800

    几种常见的算法

--- algorithms_datastructures/src/main/java/com/lzq/study/lettcode/middle/Solution3.java
+++ algorithms_datastructures/src/main/java/com/lzq/study/lettcode/middle/Solution3.java
@@ -113,9 +113,77 @@ public class Solution3 {
 
     }
 
+    Index[] memo;
+    public boolean canJump1(int[] nums) {
+        return canJumpFromPosition1(0,nums);
+    }
+    private boolean canJumpFromPosition1(int position, int[] nums){
+        if (position == nums.length - 1){
+            return true;
+        }
+        int furthestJump = Math.min(position + nums[position], nums.length - 1);
+        for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++){
+            if (canJumpFromPosition1(nextPosition,nums)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean canJump2(int[] nums) {
+        memo = new Index[nums.length];
+        for (int i = 0; i < memo.length; i++){
+            memo[i] = Index.UNKNOWN;
+        }
+        memo[memo.length - 1] = Index.GOOD;
+        return canJumpFromPosition2(0,nums);
+    }
+    private boolean canJumpFromPosition2(int position, int[] nums){
+        if (memo[position] != Index.UNKNOWN){
+            return memo[position] == Index.GOOD ? true : false;
+        }
+        int furthestJump = Math.min(position + nums[position], nums.length - 1);
+        for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++){
+            if (canJumpFromPosition2(nextPosition, nums)){
+                memo[position] = Index.GOOD;
+                return true;
+            }
+        }
+        memo[position] = Index.BAD;
+        return false;
+    }
+    public boolean canJump3(int[] nums) {
+        memo = new Index[nums.length];
+        for (int i = 0; i < memo.length; i++){
+            memo[i] = Index.UNKNOWN;
+        }
+        memo[nums.length - 1] = Index.GOOD;
+        for (int position = nums.length - 2; position >= 0; position--){
+            int furthestJump = Math.min(position + nums[position], nums.length - 1);
+            for (int j = position + 1; j <= furthestJump; j++){
+                if (memo[j] == Index.GOOD){
+                    memo[position] = Index.GOOD;
+                    break;
+                }
+            }
+        }
+        return memo[0] == Index.GOOD;
+    }
+    public boolean canJump4(int[] nums){
+        int lastPosition = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--){
+            if ( i + nums[i] >= lastPosition){
+                lastPosition = i;
+            }
+        }
+        return lastPosition == 0;
+    }
     public static void main(String[] args) {
         int[] nums = new int[]{1,1,2};
         List<List<Integer>> result = new Solution3().permuteUnique(nums);
         result.forEach(list -> System.out.println(list));
+    }
+
+    enum Index{
+        GOOD, BAD, UNKNOWN
     }
 }
