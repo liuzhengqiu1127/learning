96aafdfdc8467628e2983933f17d022d877e307f
liuzhengqiu <liuzhengqiu1127@163.com>
2019-10-18 14:39:11 +0800

    add finite state machine process

--- algorithms_datastructures/src/main/java/com/lzq/study/lettcode/middle/Solution4.java
+++ algorithms_datastructures/src/main/java/com/lzq/study/lettcode/middle/Solution4.java
@@ -170,10 +170,45 @@ public class Solution4 {
         return res[rows-1][columns-1];
     }
 
+    public boolean isNumber(String s) {
+        int state = 0;
+        int finals = 0b101101000;
+        int[][] transfer = new int[][]{{ 0, 1, 6, 2,-1},
+                {-1,-1, 6, 2,-1},
+                {-1,-1, 3,-1,-1},
+                { 8,-1, 3,-1, 4},
+                {-1, 7, 5,-1,-1},
+                { 8,-1, 5,-1,-1},
+                { 8,-1, 6, 3, 4},
+                {-1,-1, 5,-1,-1},
+                { 8,-1,-1,-1,-1}};
+        char[] ss = s.toCharArray();
+        for (int i = 0; i < ss.length; ++i){
+            int id = make(ss[i]);
+            if (id < 0) return false;
+            state = transfer[state][id];
+            if (state < 0) return false;
+        }
+        return (finals & (1 << state)) > 0;
+    }
+    public int make(char c){
+        switch (c){
+            case ' ': return 0;
+            case '+':
+            case '-': return 1;
+            case '.': return 3;
+            case 'e': return 4;
+            default:
+                if (c >= 48 && c <= 57) return 2;
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
-        int[][] grid = new int[][]{{1,2},{5,6},{1,1}};
-        int result = new Solution4().minPathSum(grid);
-        System.out.println(result);
+//        int finals = 0b101101000;
+//        System.out.println(finals);
+        int state = 4;
+        System.out.println(1 << state);
     }
 
 }
