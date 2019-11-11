d331adde34b677217212b98b60ef5c5ad091beae
liuzhengqiu <liuzhengqiu1127@163.com>
2019-11-11 17:30:15 +0800

    update

---
+++ spring_cloud/client-config/src/test/java/com/lzq/spring/cloud/config/client/ClientConfig/ClientConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzq.spring.cloud.config.client.ClientConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
