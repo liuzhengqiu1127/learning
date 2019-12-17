ce143a1ee0da8e0a4d6481b790e3f40d1ee05cf9
liuzhengqiu <liuzhengqiu1127@163.com>
2019-12-17 21:34:01 +0800

    update

--- algorithms_datastructures/src/test/java/com/lzq/study/lettcode/OneSixFourCompetition.java
+++ algorithms_datastructures/src/test/java/com/lzq/study/lettcode/OneSixFourCompetition.java
@@ -3,10 +3,7 @@ package com.lzq.study.lettcode;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 
 public class OneSixFourCompetition {
@@ -135,6 +132,59 @@ public class OneSixFourCompetition {
             result.add(everyResult);
         }
         return result;
+    }
+
+    @Test
+    public void test004(){
+        Assert.assertTrue(numWays(3,2)==4);
+        Assert.assertTrue(numWays(2,4)==2);
+        Assert.assertTrue(numWays(4,2)==8);
+    }
+
+    private static int MOD = 1_000_000_007;
+
+    private Map<String, Integer> mem = new HashMap<>();
+
+    public int numWays(int steps, int arrLen) {
+        int[][] dp = new int[steps+1][steps+1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= steps; i++){
+            for (int j = 0; j <= Math.min(i,arrLen-1);j++){
+                dp[i][j] = dp[i-1][j];
+                if (j-1>=0) {
+                    dp[i][j] += dp[i-1][j-1];
+                    dp[i][j] %= MOD;
+                }
+                if (j < i){
+                    dp[i][j] += dp[i-1][j+1];
+                    dp[i][j] %= MOD;
+                }
+            }
+        }
+        return dp[steps][0];
+//        return dp(steps,arrLen,0);
+    }
+
+    private int dp(int steps, int arrLen, int pos) {
+        // 递归终止条件，可走0步，若此时在原点则返回1
+        if (steps == 0) { return pos == 0 ? 1 : 0;}
+
+        String code = "" + steps + "," + pos;
+
+        // 之前是否到达过这个状态，是的话直接返回
+        if (mem.containsKey(code)) return mem.get(code);
+
+        int ans = dp(steps-1, arrLen, pos);  // 原地不动
+
+        //右移一步
+        if (pos+1 < arrLen) { ans += dp(steps-1, arrLen, pos+1); ans %= MOD; }
+        //左移一步
+        if (pos-1 >= 0)     { ans += dp(steps-1, arrLen, pos-1); ans %= MOD; }
+
+        // 记下当前状态，防止重复搜索
+        mem.put(code, ans);
+
+        return ans;
     }
 
 }
