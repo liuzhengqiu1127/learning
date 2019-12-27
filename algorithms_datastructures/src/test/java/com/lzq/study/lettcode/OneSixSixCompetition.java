5e4e19bfd2dff8a447cebdfd058c62af6e5e58f6
liuzhengqiu <liuzhengqiu1127@163.com>
2019-12-27 15:20:14 +0800

    update onesixsix competition

--- algorithms_datastructures/src/test/java/com/lzq/study/lettcode/OneSixSixCompetition.java
+++ algorithms_datastructures/src/test/java/com/lzq/study/lettcode/OneSixSixCompetition.java
@@ -90,4 +90,79 @@ public class OneSixSixCompetition {
         }
         return sum;
     }
+
+
+    @Test
+    public void test04(){
+//        int[][] mat = {{0,0},{0,1}};
+//        Assert.assertTrue(minFlips(mat) == 3);
+        int[][] mat1 = {{1,1,1},{1,0,1},{0,0,0}};
+        Assert.assertTrue(minFlips(mat1) == 6);
+//        int[][] mat2 = {{1,0,0},{1,0,0}};
+//        Assert.assertTrue(minFlips(mat2) == -1);
+    }
+    private int m, n;
+    private int ans = 10;
+    private int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
+    public int minFlips(int[][] mat) {
+        m = mat.length;//行数
+        n = mat[0].length;//列数
+        int diff = 0;// 需要变换的个数
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (mat[i][j] == 1){
+                    diff++;
+                }
+            }
+        }
+        dfs(mat,0,0,diff,0);
+        if (ans == 10){
+            return -1;
+        }else {
+            return ans;
+        }
+    }
+    private void dfs(int[][] mat, int i, int j, int diff, int cnt){
+        if (j == n){
+            j = 0;
+            i = i + 1;
+            dfs(mat,i,j,diff,cnt);
+            return;
+        }
+        if (diff == 0){
+            ans = Math.min(ans,cnt);
+            return;
+        }
+        if (i == m){
+            return;
+        }
+        int newDiff = help(mat,i,j);
+        dfs(mat,i,j+1,diff+newDiff,cnt+1);
+        help(mat,i,j);
+        dfs(mat,i,j+1,diff,cnt);
+
+    }
+    private int help(int[][] mat, int i, int j){
+        int cnt = 0;
+        if (mat[i][j] == 0){
+            cnt++;
+        }else {
+            cnt--;
+        }
+        mat[i][j] = 1 - mat[i][j];
+        for (int[] d : dir){
+            int nx = i + d[0], ny = j + d[1];
+            if (nx < 0 || nx >= m || ny < 0 || ny >= n){
+                continue;
+            }
+            if (mat[nx][ny] == 0){
+                cnt++;
+            }else {
+                cnt--;
+            }
+            mat[nx][ny] = 1 - mat[nx][ny];
+        }
+        return cnt;
+    }
+
 }
