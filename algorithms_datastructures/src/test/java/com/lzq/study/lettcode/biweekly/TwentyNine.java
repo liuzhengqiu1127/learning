4b73e16b58073a2a4a3f7aaf2ea130ebfc0e333e
liuzhengqiu <liuzhengqiu1127@163.com>
2020-06-28 22:50:09 +0800

    update

--- algorithms_datastructures/src/test/java/com/lzq/study/lettcode/biweekly/TwentyNine.java
+++ algorithms_datastructures/src/test/java/com/lzq/study/lettcode/biweekly/TwentyNine.java
@@ -2,8 +2,10 @@ package com.lzq.study.lettcode.biweekly;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.stream.Collectors;
 
 public class TwentyNine {
 
@@ -78,11 +80,7 @@ public class TwentyNine {
 
     @Test
     public void test(){
-        System.out.println(longestSubarray(new int[]{1,1,0,1}));
-        System.out.println(longestSubarray(new int[]{0,1,1,1,0,1,1,0,1}));
-        System.out.println(longestSubarray(new int[]{1,1,1}));
-        System.out.println(longestSubarray(new int[]{1,1,0,0,1,1,1,0,1}));
-        System.out.println(longestSubarray(new int[]{0,0,0}));
+        System.out.println(minNumberOfSemesters(11,new int[][]{},2));
     }
 
     public int minNumberOfSemesters(int n, int[][] dependencies, int k) {
@@ -90,24 +88,49 @@ public class TwentyNine {
             return n%k==0 ? n/k : (n/k + 1);
         }
 
-        List<Tree> root = new ArrayList<>();
+        Map<Integer,List<Integer>> record = new HashMap<>();
 
         for (int i=0; i<dependencies.length; i++){
             int child = dependencies[i][0];
             int parent = dependencies[i][1];
-
+            if (record.containsKey(parent)&&record.containsKey(child)){
+                record.get(parent).add(child);
+            }else if (record.containsKey(parent)&&!record.containsKey(child)){
+                record.get(parent).add(child);
+                record.put(child,new ArrayList<>());
+            }else if (!record.containsKey(parent)&&!record.containsKey(child)){
+                List<Integer> childList = new ArrayList<>();
+                childList.add(child);
+                record.put(parent,childList);
+                record.put(child,new ArrayList<>());
+            } else{
+                List<Integer> childList = new ArrayList<>();
+                childList.add(child);
+                record.put(parent,childList);
+            }
         }
 
-        return 0;
+        int sum = 0;
+        List<Integer> yeZiList = record.entrySet().stream().filter(integerListEntry -> integerListEntry.getValue().isEmpty()).map(integerListEntry -> integerListEntry.getKey()).collect(Collectors.toList());
+        sum += (yeZiList.size()%k==0)?(yeZiList.size()/k):(yeZiList.size()/k+1);
+        Queue<List<Integer>> queue = new LinkedBlockingQueue<>();
+        queue.add(yeZiList);
+        while (queue.isEmpty()){
+            List<Integer> popList = queue.poll();
+            List<Integer> parentList = record.entrySet().stream()
+                    .filter(integerListEntry -> isContain(integerListEntry.getValue(), popList))
+                    .map(integerListEntry -> integerListEntry.getKey())
+                    .collect(Collectors.toList());
+            if (parentList.isEmpty()) break;
+            sum += (parentList.size()%k==0)?(parentList.size()/k):(parentList.size()/k+1);
+            queue.add(parentList);
+        }
+        return sum;
 
     }
 
-    class Tree{
-        int value;
-        List<Tree> treeList;
-        public Tree(int value){
-            this.value = value;
-        }
+    private boolean isContain(List<Integer> childList, List<Integer> recordList){
+        return recordList.stream().filter(integer -> childList.contains(integer)).findAny().isPresent();
     }
 
 }
