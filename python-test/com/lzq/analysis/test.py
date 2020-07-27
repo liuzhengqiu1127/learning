c4b02140af4d00d49db5235f8dbc70a28f6f3b28
liuzhengqiu <liuzhengqiu1127@163.com>
2020-07-27 20:18:24 +0800

    update

--- python-test/com/lzq/analysis/test.py
+++ python-test/com/lzq/analysis/test.py
@@ -1,15 +1,19 @@
+from filecmp import cmp
+from functools import cmp_to_key
+
 import numpy as np
 
 if __name__ == '__main__':
     person_type = np.dtype({
-        'names': ['name', 'age', 'chinese', 'math', 'english'],
+        'names': ['name', 'age', 'chinese', 'english', 'math'],
         'formats': ['S32', 'i', 'i', 'i', 'f']
     })
 
-    peoples = np.array([("ZhangFei", 32, 75, 100, 90),
-                        ("GuanYu", 24, 85, 96, 88.5),
-                        ("ZhaoYun", 28, 85, 92, 96.5),
-                        ("HuangZhong", 29, 65, 85, 100)
+    peoples = np.array([("ZF", 32, 66, 65, 30),
+                        ("GY", 24, 95, 85, 98),
+                        ("ZY", 28, 93, 92, 96),
+                        ("HZ", 29, 90, 88, 77),
+                        ("WD", 29, 80, 90, 90)
                         ], dtype=person_type)
 
     ages = peoples[:]['age']
@@ -17,8 +21,10 @@ if __name__ == '__main__':
     maths = peoples[:]['math']
     englishs = peoples[:]['english']
 
-    print(np.mean(ages))
-    print(np.mean(chineses))
-    print(np.mean(maths))
-    print(np.mean(englishs))
-    
+    print("语文平均成绩={},最小成绩={}，最大成绩={}，方差={}，标准差={}".format(
+        np.mean(chineses), np.min(chineses), np.max(chineses), np.var(chineses), np.std(chineses)))
+
+    ranking = sorted(peoples, key=cmp_to_key(lambda x, y: int(x[2]+x[3]+x[4])-int(y[2]+y[3]+y[4])), reverse=True)
+    print(ranking)
+
+
--- python-test/com/lzq/analysis/ufuncTest.py
+++ python-test/com/lzq/analysis/ufuncTest.py
@@ -23,7 +23,12 @@ def test002():
     print(np.amax(a, 2))  # 求 每个数组
 
 
+def test003():
+    a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    print(np.ptp(a))
+    print(np.ptp(a, 0))
+    print(np.ptp(a, 1))
 
 
 if __name__ == '__main__':
-    test002()
+    test003()
