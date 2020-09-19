225f0518e50cc53767a4d333732c3c4c343b429a
liuzhengqiu <liuzhengqiu1127@163.com>
2020-09-19 16:01:09 +0800

    update

--- spring_cloud/comm-app/data-acquisition/src/main/java/com/lzq/spring/cloud/acquisition/AcquisitionApplication.java
+++ spring_cloud/comm-app/data-acquisition/src/main/java/com/lzq/spring/cloud/acquisition/AcquisitionApplication.java
@@ -1,13 +1,13 @@
-package com.lzq.spring.cloud.springbatch;
+package com.lzq.spring.cloud.acquisition;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBatchApplication {
+public class AcquisitionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBatchApplication.class, args);
+		SpringApplication.run(AcquisitionApplication.class, args);
 	}
 
 }
--- spring_cloud/comm-app/data-acquisition/src/test/java/com/lzq/spring/cloud/acquisition/AcquisitionApplicationTests.java
+++ spring_cloud/comm-app/data-acquisition/src/test/java/com/lzq/spring/cloud/acquisition/AcquisitionApplicationTests.java
@@ -1,10 +1,10 @@
-package com.lzq.spring.cloud.springbatch;
+package com.lzq.spring.cloud.acquisition;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBatchApplicationTests {
+class AcquisitionApplicationTests {
 
 	@Test
 	void contextLoads() {
