632127849f6c36196a213ff03096b667431045c0
liuzhengqiu <liuzhengqiu1127@163.com>
2020-09-28 19:58:50 +0800

    update

---
+++ algorithms_datastructures/src/test/java/com/lzq/study/lettcode/weekly/twozero/TwoZeroEight.java
@@ -0,0 +1,70 @@
+package com.lzq.study.lettcode.weekly.twozero;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TwoZeroEight {
+
+    public int minOperations(String[] logs) {
+        int home = 0;
+        for (String log : logs){
+            if (log.equalsIgnoreCase("./")){
+                continue;
+            }else if (log.equalsIgnoreCase("../")){
+                home = (home > 0 ? home-1 : home);
+            }else{
+                home++;
+            }
+        }
+        return home;
+    }
+
+    public int minOperationsMaxProfit(int[] customers, int boardingCost, int runningCost) {
+        if (4 * boardingCost - runningCost <= 0) return -1;
+        List<Integer> record = new ArrayList<>();
+        int remain = 0;
+        int profit = 0;
+        for (int customer : customers){
+            int current = remain + customer;
+            if (current >= 4){
+                profit += (4*boardingCost-runningCost);
+                remain = current - 4;
+            }else{
+                profit += (current*boardingCost - runningCost);
+                remain = 0;
+            }
+            record.add(profit);
+        }
+        while (remain > 0){
+            if (remain >= 4){
+                profit += (4*boardingCost - runningCost);
+                remain = remain - 4;
+            }else {
+                profit += (remain*boardingCost - runningCost);
+                remain = 0;
+            }
+            record.add(profit);
+        }
+        int max = 0;
+        int count = 0;
+        int result = 0;
+        for (int red : record){
+            count++;
+            if (red > max){
+                result = count;
+                max = red;
+            }
+        }
+        return result == 0 ? - 1 : result;
+    }
+
+    @Test
+    public void test(){
+        int[] customers = {24,0,32,1,39,33,36,40,22,40,5,43,31,27,4,0,11,17,5,10,11,19,35,50,20,18,49,4,46,8,50,0,4,19,12,19,37,10,0,27,45,1,23,46,48,26,12,34,19,25,49,1,36,14,43,31,7,14,46,0,1,23,40};
+        System.out.println(minOperationsMaxProfit(customers,22,53));
+    }
+
+}
