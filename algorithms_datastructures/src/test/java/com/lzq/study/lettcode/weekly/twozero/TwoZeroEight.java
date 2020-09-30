1a3e65684719f99592f11cb3f3934df85c3a9785
liuzhengqiu <liuzhengqiu1127@163.com>
2020-09-30 21:23:27 +0800

    update

--- algorithms_datastructures/src/test/java/com/lzq/study/lettcode/weekly/twozero/TwoZeroEight.java
+++ algorithms_datastructures/src/test/java/com/lzq/study/lettcode/weekly/twozero/TwoZeroEight.java
@@ -97,4 +97,36 @@ public class TwoZeroEight {
         }
     }
 
+    int max = 0;
+    public int maximumRequests(int n, int[][] requests) {
+        int count = 0;
+        int[] lou = new int[n];
+        helper(requests, lou, count, 0);
+        return max;
+    }
+    public void helper(int[][] requests, int[] lou, int count, int index){
+        if(check(lou)){
+            max = Math.max(max, count);
+        }
+        for(int i=index;i<requests.length;++i){
+            // 选择这个requests
+            lou[requests[i][0]]--;
+            lou[requests[i][1]]++;
+            count++;
+
+            // 不选择这个requests 
+            helper(requests, lou, count, i+1);
+            lou[requests[i][0]]++;
+            lou[requests[i][1]]--;
+            count--;
+
+        }
+    }
+    public boolean check(int[] lou){
+        for(int i=0;i<lou.length;++i){
+            if(lou[i] != 0) return false;
+        }
+        return true;
+    }
+
 }
