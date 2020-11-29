f0aec8be22d9a09fe202c8310999a93fc034037c
liuzhengqiu <liuzhengqiu1127@163.com>
2020-11-29 11:21:15 +0800

    biweekly 40 competition

--- algorithms_datastructures/src/test/java/com/lzq/study/lettcode/biweekly/Forty.java
+++ algorithms_datastructures/src/test/java/com/lzq/study/lettcode/biweekly/Forty.java
@@ -40,24 +40,47 @@ public class Forty {
         return head;
     }
 
+    /**
+     *  index = i时， delLeft[i] 表示左边最少删除多少个
+     *  delLeft[i] = min ( delLeft[i], delLeft[j] + i - j - 1)
+     *  delRight[i] = min ( delRight[i], delRight[j] + j - i - 1)
+     * @param nums
+     * @return
+     */
     public int minimumMountainRemovals(int[] nums) {
-        int deleteNumber = Integer.MAX_VALUE;
-
-        for (int i=1; i < nums.length-1; i++){
-            int deleteCount = 0;
-            for (int j = 0; j < i; j++){
-                
-            }
-
-
+        int n = nums.length;
+        int[] delLeft = new int[n];
+        int[] delRight = new int[n];
+        for (int i = 0; i < n; i++){
+            delLeft[i] = i;
+            delRight[i] = n - i - 1;
         }
 
-        return deleteNumber;
+        for (int i = 0; i < n; i++){
+            for (int j = i-1; j>=0; j--){
+                if (nums[j] < nums[i]){ // 左边
+                    delLeft[i] = Integer.min(delLeft[i],delLeft[j]+i-j-1);
+                }
+            }
+        }
+        for (int i = n - 1; i >= 0; i--){
+            for (int j = i + 1; j < n; j++){
+                if (nums[j] < nums[i]){
+                    delRight[i] = Integer.min(delRight[i], delRight[j]+j-i-1);
+                }
+            }
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int i = 1; i < n - 1; i++){
+            ans = Integer.min(ans, delLeft[i] + delRight[i]);
+        }
+        return ans;
     }
+
 
     @Test
     public void test(){
-
+        System.out.println(minimumMountainRemovals(new int[]{4,3,2,1,1,2,3,1}));
     }
 
 }
