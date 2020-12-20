ac1e28641b62772c4958078fb1a21d8d42be784c
liuzhengqiu <liuzhengqiu1127@163.com>
2020-12-20 18:51:26 +0800

    add 220 completition

--- algorithms_datastructures/src/test/java/com/lzq/study/lettcode/weekly/twotwo/TwoTwoZero.java
+++ algorithms_datastructures/src/test/java/com/lzq/study/lettcode/weekly/twotwo/TwoTwoZero.java
@@ -32,6 +32,36 @@ public class TwoTwoZero {
         return result.toString();
     }
 
+    public int maximumUniqueSubarray(int[] nums){
+        int[] array = new int[nums.length + 1];
+        int maxLength = 0;
+        for (int i = 0; i < nums.length; i++){
+            array[i+1] = array[i] + nums[i];
+            maxLength = Integer.max(maxLength, nums[i]);
+        }
+        int max = 0;
+        boolean[] sign = new boolean[maxLength + 1];
+        int start = 0;
+        int end = 0;
+        while (end < nums.length){
+            for (; end < nums.length; end++){
+                if (sign[nums[end]]) break;
+                sign[nums[end]] = true;
+            }
+            int sum = array[end] - array[start];
+            max = Integer.max(max,sum);
+            if (end < nums.length){
+                while (start < end && nums[end] != nums[start]){
+                    sign[nums[start]] = false;
+                    start++;
+                }
+                sign[nums[start]] = false;
+                start++;
+            }
+        }
+        return max;
+    }
+
     @Test
     public void test(){
         System.out.println(reformatNumber("--17-5 229 35-39475 "));
